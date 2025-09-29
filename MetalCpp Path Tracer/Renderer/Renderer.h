a6e81a17d637957fd0004b3e77a9dabe0489e774
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-09-29 16:17:48 +0200

    Revert "Fix PendingBufferHandles definition in header"

--- MetalCpp Path Tracer/Renderer/Renderer.h
+++ MetalCpp Path Tracer/Renderer/Renderer.h
@@ -85,19 +85,7 @@ private:
 
   struct CompactionBuildInput;
   struct CompactionResultData;
-  struct PendingBufferHandles {
-    MTL::Buffer *primitive = nullptr;
-    MTL::Buffer *material = nullptr;
-    MTL::Buffer *primitiveIndices = nullptr;
-    MTL::Buffer *bvh = nullptr;
-    MTL::Buffer *triangleVertices = nullptr;
-    MTL::Buffer *triangleIndices = nullptr;
-    MTL::Buffer *remap = nullptr;
-    MTL::Buffer *activeMask = nullptr;
-    MTL::Buffer *instance = nullptr;
-    MTL::Buffer *lightIndices = nullptr;
-    MTL::Buffer *lightCdf = nullptr;
-  };
+  struct PendingBufferHandles;
   void launchCompactionBuild(CompactionBuildInput &&input);
   std::shared_ptr<CompactionResultData>
   buildCompactionResult(CompactionBuildInput input) const;
