2faaa0221c7ab25647a0a142b46c4f97eb0f02bd
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-10-01 21:29:50 +0200

    Fix Metal pointer conversion and TLAS binding

--- MetalCpp Path Tracer/Renderer/Shaders/PathTracing.h
+++ MetalCpp Path Tracer/Renderer/Shaders/PathTracing.h
@@ -49,7 +49,7 @@ inline device const uchar *addressAsPointer(uint64_t address) {
   if (address == 0)
     return nullptr;
 #if defined(__METAL_VERSION__)
-  return static_cast<device const uchar *>(__builtin_int_to_ptr(address));
+  return reinterpret_cast<device const uchar *>(static_cast<ulong>(address));
 #else
   return reinterpret_cast<device const uchar *>(address);
 #endif
