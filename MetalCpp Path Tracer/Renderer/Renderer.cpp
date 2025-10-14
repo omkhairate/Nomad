6251cb395a41940952b42b60d627a4592279249d
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-10-15 01:58:56 +0200

    changes

--- MetalCpp Path Tracer/Renderer/Renderer.cpp
+++ MetalCpp Path Tracer/Renderer/Renderer.cpp
@@ -1221,7 +1221,7 @@ void Renderer::buildShaders() {
 void Renderer::updateVisibleScene() {
   if (!SceneLoader::LoadSceneFromXML("scene.xml", _pScene)) {
     std::filesystem::path alt =
-        std::filesystem::path(__FILE__).parent_path() / "../scene_staircase_alwaysresident.xml";
+        std::filesystem::path(__FILE__).parent_path() / "../staircase.xml";
     SceneLoader::LoadSceneFromXML(alt.string(), _pScene);
   }
 
