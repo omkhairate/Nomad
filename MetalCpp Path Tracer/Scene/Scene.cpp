83c1330500ba36c0ef436141e11359c3bbae2be3
Om Khairate <72100111+omkhairate@users.noreply.github.com>
2025-10-29 06:50:16 +0100

    Revert "Revert "Revert "Restore environment lighting support"""

--- MetalCpp Path Tracer/Scene/Scene.cpp
+++ MetalCpp Path Tracer/Scene/Scene.cpp
@@ -206,7 +206,9 @@ void Scene::setEnvironmentBrightness(float brightness) {
   environment.brightness = std::max(brightness, 0.0f);
 }
 
-bool Scene::hasEnvironmentTexture() const { return !environment.texturePath.empty(); }
+bool Scene::hasEnvironmentTexture() const {
+  return !environment.texturePath.empty();
+}
 
 ResidencyStrategy Scene::getResidencyStrategy() const {
   return residencyStrategy;
